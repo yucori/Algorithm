75013bd1b5d67c4532605cf1bd2642d55b7e2884
jang_jiwon <kooriangman@gmail.com>
2023-05-21 14:32:23 +0900

    12906 c++ 풀이

---
+++ Level 1/12906.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    answer.push_back(arr[0]);
+    for(int i=0; i<arr.size()-1; i++){
+        if(arr[i]!=arr[i+1]){
+            answer.push_back(arr[i+1]);
+        }
+    }
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
