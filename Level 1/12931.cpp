aa8ee091728441c11e5432d31d4fafa18fdbf26b
jang_jiwon <kooriangman@gmail.com>
2023-06-03 15:32:14 +0900

    12931 c++ 풀이

---
+++ Level 1/12931.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    while(n/10>=1){
+        answer+=n%10;
+        n = n/10;
+    }
+    answer+=n;
+
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
