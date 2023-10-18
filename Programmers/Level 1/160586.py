6585a09dc4d65c8f596a86ac622e93075444b450
jang_jiwon <kooriangman@gmail.com>
2023-10-18 15:14:22 +0900

    [Programmers] 160586 python3 풀이

---
+++ Programmers/Level 1/160586.py
@@ -0,0 +1,32 @@
+keymap = ["AA"]
+targets = ["B"]
+# results = [9,4]
+answer = []
+keymap_list = []
+
+for i in range(len(keymap)):
+    keymap_list.append(list(keymap[i]))
+
+for i in range(len(targets)):
+    cnt = 0
+    for j in range(len(targets[i])):
+        k = 0
+        temp = -1
+        while k != len(keymap_list):
+            if targets[i][j] in keymap_list[k]:
+                if temp != -1 and temp > keymap_list[k].index(targets[i][j]):
+                    temp = keymap_list[k].index(targets[i][j])
+                elif temp == -1:
+                    temp = keymap_list[k].index(targets[i][j])
+            k += 1
+        print("targets[i][j]: ", end="")
+        print(targets[i][j])
+        print(temp)
+        if temp == -1:
+            cnt = -1
+            break
+        else:
+            cnt += (temp + 1)
+    answer.append(cnt)
+
+print(answer)
