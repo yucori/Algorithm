270d62b208e6eda8c301fec051d03e0fd21b43e8
jang_jiwon <kooriangman@gmail.com>
2024-02-06 17:53:03 +0900

    [Baekjoon] 15651 c++ 풀이

---
+++ Baekjoon/15651.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int n, m;
+int arr[13];
+bool visited[13];
+void dfs(int cnt);
+int main()
+{
+    cin >> n >> m;
+    dfs(0);
+}
+void dfs(int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (!visited[i])
+        {
+            arr[cnt] = i;
+            dfs(cnt + 1);
+            visited[i] = false;
+        }
+    }
+}
